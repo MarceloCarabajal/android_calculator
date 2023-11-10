935130974e0338d305ee7ffad88ce729630929ac
MarceloCarabajal <marce.tsgia@gmail.com>
2023-11-10 02:57:49 -0300

    activity_main + themes + layout + strings + menu + dimens

---
+++ app/src/main/java/curso/marce/mycalculator/MainActivity.java
@@ -0,0 +1,25 @@
+package curso.marce.mycalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
